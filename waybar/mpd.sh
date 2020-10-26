266c803a3f75244452e7f9a0743776a482ec446e
Alejandro Rodriguez <alejometal@gmail.com>
2020-10-26 16:03:34 -0500

    Fix waybay mpd issue with songs incorrectly formatted

--- waybar/mpd.sh
+++ waybar/mpd.sh
@@ -1,3 +1,2 @@
 #!/bin/zsh
-echo 'currentsong\nstatus' | ncat localhost 6600 | grep : | sed  's/: /=/' | jc --env -r | jq '. | if .state != "play" then "mpd", .state else .Artist, ":", .Title, "\n" ,.file, .audio, .bitrate, "kbps"  end' | xargs echo -e
-
+echo 'currentsong\nstatus' | ncat localhost 6600 | grep : | sed  's/: /=/' | jc --env -r | jq '. | if .state != "play" then "mpd", .state else .Artist, ":", .Title, "\n" ,.file, .audio, .bitrate, "kbps"  end' | xargs echo -e || echo "no name"
